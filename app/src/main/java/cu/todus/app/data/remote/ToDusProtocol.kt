d92dabf8ba1a1e52fa3a7541b5c7f20513c89589
miguel-dev19 <migueldesarrollador374@gmail.com>
2026-07-19 14:18:29 -0400

    Protocolo completo según guía oficial: edit/delete/forward, multimedia builders, grupos IQ, privacidad, bloqueo

--- app/src/main/java/cu/todus/app/data/remote/ToDusProtocol.kt
+++ app/src/main/java/cu/todus/app/data/remote/ToDusProtocol.kt
@@ -5,7 +5,7 @@ import java.util.Base64
 object ToDusProtocol {
     const val STREAM_NS = "jc"
     const val STREAM_XMLNS = "x1"
-    const val SASL_NS = "urn:ietf:params:xml:ns:xmpp-sasl"  // ESTÁNDAR - el servidor responde <ok xmlns='x2'/>
+    const val SASL_NS = "urn:ietf:params:xml:ns:xmpp-sasl"
     const val BIND_NS = "urn:ietf:params:xml:ns:xmpp-bind"
     const val SESSION_NS = "x5"
     const val SESSION_ELEM = "s1"
@@ -38,6 +38,9 @@ object ToDusProtocol {
     fun buildOutgoingMessage(to: String, body: String, msgId: String = randomHex(16)): String =
         "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><b>${escapeXml(body)}</b></m>"
     
+    fun buildGroupMessage(groupJid: String, body: String, msgId: String = randomHex(16)): String =
+        "<m o=\"$groupJid\" t=\"gc\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><b>${escapeXml(body)}</b></m>"
+    
     fun buildReceivedReceipt(to: String, originalMsgId: String): String {
         val ackId = randomHex(16)
         return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$ackId\" xmlns=\"$MSG_NS\"><rd xmlns=\"$BODY_K_NS\" i=\"$originalMsgId\"/></m>"
@@ -53,6 +56,17 @@ object ToDusProtocol {
     
     fun buildComposingStopped(to: String): String =
         "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"${randomHex(16)}\" xmlns=\"$MSG_NS\"><csc xmlns=\"uc1\"/></m>"
+    
+    fun buildEditedMessage(to: String, originalMsgId: String, newBody: String): String =
+        "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$originalMsgId\" xmlns=\"$MSG_NS\"><edited xmlns=\"edited:n\" i=\"edit_${randomHex(8)}\" mi=\"$originalMsgId\"/><k xmlns=\"$BODY_K_NS\"/><b>${escapeXml(newBody)}</b></m>"
+    
+    fun buildDeletedMessage(to: String, originalMsgId: String): String =
+        "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$originalMsgId\" xmlns=\"$MSG_NS\"><deleted xmlns=\"deleted:n\" i=\"del_${randomHex(8)}\" mi=\"$originalMsgId\"/><k xmlns=\"$BODY_K_NS\"/><b/></m>"
+    
+    fun buildForwardedMessage(to: String, originalMsg: ToDusMessage, replyBody: String): String {
+        val newMsgId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$newMsgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><forwarded xmlns=\"urn:xmpp:forward:0\"><delay xmlns=\"urn:xmpp:delay\" stamp=\"${originalMsg.timestamp}\"/><m xmlns=\"$MSG_NS\" o=\"${originalMsg.to}\" f=\"${originalMsg.from}\" i=\"${originalMsg.id}\" t=\"c\"><b>${escapeXml(originalMsg.body)}</b><k xmlns=\"$BODY_K_NS\"/></m></forwarded><b>${escapeXml(replyBody)}</b></m>"
+    }
     
     fun buildOfflineIq(): String = "<iq t=\"get\" i=\"off_${randomHex(8)}\"><query xmlns=\"t:offline\"/></iq>"
     
@@ -73,8 +87,17 @@ object ToDusProtocol {
     fun buildBlockUserIq(phone: String): String =
         "<iq t=\"set\" i=\"blk_${randomHex(8)}\" to=\"$DOMAIN\"><query xmlns=\"todus:block:set\" jid=\"$phone@$DOMAIN\"/></iq>"
     
+    fun buildUnblockUserIq(phone: String): String =
+        "<iq t=\"set\" i=\"unblk_${randomHex(8)}\" to=\"$DOMAIN\"><query xmlns=\"todus:block:unset\" jid=\"$phone@$DOMAIN\"/></iq>"
+    
     fun buildRosterDeleteIq(phone: String): String =
         "<iq t=\"set\" i=\"del_${randomHex(8)}\"><query xmlns=\"todus:roster:delete\" jid=\"$phone@$DOMAIN\"/></iq>"
+    
+    fun buildPrivacyQueryIq(): String =
+        "<iq t=\"get\" i=\"priv_${randomHex(8)}\"><query xmlns=\"todus:privacy\"/></iq>"
+    
+    fun buildPrivacyUpdateIq(info: String, last: String, photo: String): String =
+        "<iq t=\"set\" i=\"privu_${randomHex(8)}\"><query xmlns=\"todus:privacy\" info=\"$info\" last=\"$last\" profile_photo=\"$photo\"/></iq>"
     
     fun buildS3UploadIq(fileType: Int, size: Long): String =
         "<iq t=\"get\" i=\"s3_${randomHex(8)}\"><query xmlns=\"todus:purl\" type=\"$fileType\" persistent=\"true\" size=\"$size\" room=\"\"/></iq>"
@@ -82,6 +105,58 @@ object ToDusProtocol {
     fun buildMyGroupsIq(): String =
         "<iq t=\"get\" i=\"grp_${randomHex(8)}\" to=\"muclight.im.todus.cu\"><query xmlns=\"todus:muclight:my_mucs:2\" limit=\"100\" offset=\"0\"/></iq>"
     
+    fun buildGroupInfoIq(groupJid: String): String =
+        "<iq t=\"get\" i=\"gi_${randomHex(8)}\" to=\"$groupJid\"><query xmlns=\"td:g:info_by_id\"/></iq>"
+    
+    fun buildGroupMembersIq(groupJid: String): String =
+        "<iq t=\"get\" i=\"gm_${randomHex(8)}\" to=\"$groupJid\"><query xmlns=\"x11\"/></iq>"
+    
+    fun buildGroupLinkIq(groupJid: String): String =
+        "<iq t=\"get\" i=\"gl_${randomHex(8)}\" to=\"$groupJid\"><query xmlns=\"x14\"/></iq>"
+    
+    fun buildCreateGroupIq(groupJid: String, name: String, description: String = ""): String =
+        "<iq t=\"set\" i=\"cg_${randomHex(8)}\" to=\"$groupJid\"><query xmlns=\"x16\"><configuration><roomname>${escapeXml(name)}</roomname><subject>${escapeXml(description)}</subject></configuration><occupants/></query></iq>"
+    
+    fun buildAddGroupMemberIq(groupJid: String, phone: String): String =
+        "<iq t=\"set\" i=\"add_${randomHex(8)}\" to=\"$groupJid\"><query xmlns=\"td:g:add_occupant\" jid=\"$phone@$DOMAIN\"/></iq>"
+    
+    // ⭐ Multimedia builders
+    fun buildImageMessage(to: String, s3Url: String, fileName: String, size: Long, width: Int = 800, height: Int = 600, thumbnail: String = ""): String {
+        val msgId = randomHex(16); val fileId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><image xmlns=\"image:n\" i=\"$fileId\" mi=\"$msgId\" url=\"$s3Url\" n=\"${escapeXml(fileName)}\" s=\"$size\" h=\"\" w=\"$width\" he=\"$height\" tnail=\"$thumbnail\"/><b>${escapeXml(fileName)}</b></m>"
+    }
+    
+    fun buildVideoMessage(to: String, s3Url: String, fileName: String, size: Long, duration: Int, width: Int = 1920, height: Int = 1080, thumbnail: String = ""): String {
+        val msgId = randomHex(16); val fileId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><video xmlns=\"video:n\" i=\"$fileId\" mi=\"$msgId\" url=\"$s3Url\" n=\"${escapeXml(fileName)}\" s=\"$size\" h=\"\" d=\"$duration\" w=\"$width\" he=\"$height\" tnail=\"$thumbnail\"/><b>${escapeXml(fileName)}</b></m>"
+    }
+    
+    fun buildAudioMessage(to: String, s3Url: String, fileName: String, size: Long, duration: Int): String {
+        val msgId = randomHex(16); val fileId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><audio xmlns=\"audio:n\" i=\"$fileId\" mi=\"$msgId\" url=\"$s3Url\" s=\"$size\" h=\"\" d=\"$duration\" n=\"${escapeXml(fileName)}\"/><b>${escapeXml(fileName)}</b></m>"
+    }
+    
+    fun buildVoiceMessage(to: String, s3Url: String, size: Long, duration: Int, waveform: String = ""): String {
+        val msgId = randomHex(16); val fileId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><voice xmlns=\"voice:n\" i=\"$fileId\" mi=\"$msgId\" url=\"$s3Url\" s=\"$size\" h=\"\" d=\"$duration\" ws=\"$waveform\"/><b/></m>"
+    }
+    
+    fun buildFileMessage(to: String, s3Url: String, fileName: String, size: Long): String {
+        val msgId = randomHex(16); val fileId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><file xmlns=\"file:n\" i=\"$fileId\" mi=\"$msgId\" n=\"${escapeXml(fileName)}\" url=\"$s3Url\" s=\"$size\" h=\"\"/><b>${escapeXml(fileName)}</b></m>"
+    }
+    
+    fun buildStickerMessage(to: String, stickerName: String, packId: String, hash: String = ""): String {
+        val msgId = randomHex(16); val fileId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><sticker xmlns=\"sticker:n\" i=\"$fileId\" mi=\"$msgId\" n=\"${escapeXml(stickerName)}\" f=\"$packId\" url=\"\" s=\"0\" h=\"$hash\" json=\"\"/><b/></m>"
+    }
+    
+    fun buildContactMessage(to: String, contactName: String, contactPhone: String): String {
+        val msgId = randomHex(16); val fileId = randomHex(16)
+        return "<m o=\"$to@$DOMAIN\" t=\"c\" i=\"$msgId\" xmlns=\"$MSG_NS\"><k xmlns=\"$BODY_K_NS\"/><contact xmlns=\"contact:n\" i=\"$fileId\" mi=\"$msgId\" n=\"${escapeXml(contactName)}\" num=\"$contactPhone\"/><b/></m>"
+    }
+    
+    // ⭐ Parsers
     fun parseIncomingMessage(xml: String): ToDusMessage? {
         return try {
             val id = extractAttribute(xml, "i") ?: return null
